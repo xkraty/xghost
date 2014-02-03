a6a7ce0b97d489830591ea1af0356c9ecf1c50a0
xKraty <kry619@gmail.com>
2014-02-04 00:20:03 +0100

    restored deleted files

--- views/login.php
+++ views/login.php
@@ -1,26 +1,24 @@
-<form action="index.php?a=login" method="post" class="login">
-  <div class="section-content">
-    <div class="wrap">
-      <div class="container">
-        <div class="form-wrap">
-          <div class="row">
-            <div class="animated bounceIn">
-              <div class="form-header">
-                <img src="img/small_logo_white.png" alt="Call of Duty Ghosts" class="logo fadeInDown animated">
-              </div>
-              <div class="form-main">
-                <form>
-                  <div class="form-group">
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="required">
-                    <input type="password" name="password" id="password" class="form-control" placeholder="Password" required="required">
-                  </div>
-                  <button type="submit" class="btn btn-block signin">Sign In</button>
-                </form>
-              </div>
-              <div class="form-footer">
-                <div class="row">
-                  Call of Duty Ghosts Web Powered by xKraty
+<form action="index.php?a=login" method="post" class="login box">
+  <div class="wrap">
+    <div class="container">
+      <div class="form-wrap">
+        <div class="row">
+          <div class="animated bounceIn">
+            <div class="form-header">
+              <img src="img/small_logo_white.png" alt="Call of Duty Ghosts" class="logo fadeInDown animated">
+            </div>
+            <div class="form-main">
+              <form>
+                <div class="form-group">
+                  <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="required">
+                  <input type="password" name="password" id="password" class="form-control" placeholder="Password" required="required">
                 </div>
+                <button type="submit" class="btn btn-block signin">Sign In</button>
+              </form>
+            </div>
+            <div class="form-footer">
+              <div class="row">
+                Call of Duty Ghosts Web Powered by xKraty
               </div>
             </div>
           </div>
